f1190778c8d5c0c8d43424f4079740aef7a08c82
ArminasAcas <arminasprollas@gmail.com>
2023-08-17 23:45:16 +0300

    added note list display based on note array

---
+++ javascript/Global.js
@@ -0,0 +1,28 @@
+let categories = {
+    general: "General",
+    work: "Work",
+    studies: "Studies",
+    health: "Health"
+
+}
+
+let notes = [
+    {
+        category: categories.general,
+        name: "Shopping list",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 0
+    },
+    {
+        category: categories.general,
+        name: "Movies to watch",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 1
+    },
+    {
+        category: categories.general,
+        name: "Ideas",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        id: 2
+    },
+]
---
+++ javascript/Notes.js
@@ -0,0 +1,40 @@
+function displayNoteList(category) {
+
+    let noteListSection = document.querySelector(".note-list__notes");
+    if (!noteListSection) return;
+
+    let filteredNotes = filterNotes(category);
+    if (!filterNotes) return;
+
+    filteredNotes.forEach(note => createNote(note,noteListSection))
+}
+
+function filterNotes(category) {
+    let filteredNotes = notes.filter( note => {
+        return note.category === category;
+    })
+
+    return filteredNotes;
+}
+
+function createNote(note,noteListSection) {
+    if(!note || !noteListSection) return;
+    
+    let div = document.createElement("div");
+    div.classList.add("note-list__note");
+    div.setAttribute("data-noteID",note.id);
+
+    let span = document.createElement("span");
+    span.innerHTML = note.name;
+
+    let button = document.createElement("button");
+    button.innerHTML="-";
+    button.classList.add("note-list__button");
+    button.classList.add("note-list__button--red");
+
+    let divider = document.createElement("div");
+    divider.classList.add("divider");
+
+    div.append(span,button);
+    noteListSection.append(div,divider);
+}
